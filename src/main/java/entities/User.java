e5fe77b97f41eb8be1ec9aa4362aee1e48a1c178
YulianaYershova <juliana.ershova97@gmail.com>
2018-08-15 00:05:00 +0300

    Create entities

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,102 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Created by Julia on 27.07.2018
+ */
+public class User implements Serializable {
+
+    private int id;
+    private UserRole role;
+    private String name;
+    private String login;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, UserRole role, String name, String login, String password) {
+        this.id = id;
+        this.role = role;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public User(UserRole role, String name, String login, String password) {
+        this.role = role;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(role, user.role) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role, name, login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", role=" + role +
+                ", name='" + name + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
