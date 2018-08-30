9297884807f51dcab0ea8c3bcd3532715116ca7a
YulianaYershova <juliana.ershova97@gmail.com>
2018-08-30 13:16:33 +0300

    Create transaction manager

--- src/main/java/persistence/dao/mySqlDAOImpl/AbstractDAO.java
+++ src/main/java/persistence/dao/mySqlDAOImpl/AbstractDAO.java
@@ -1,9 +1,7 @@
 package persistence.dao.mySqlDAOImpl;
 
 
-import logging.LoggerLoader;
-import org.apache.log4j.Logger;
-import persistence.dao.dataSource.DataSource;
+import persistence.dao.dataSource.MySqlDataSource;
 import persistence.dao.Mapper;
 
 import java.sql.*;
@@ -12,27 +10,28 @@ import java.sql.*;
  * Created by Julia on 09.08.2018
  */
 public abstract class AbstractDAO {
-    private static final Logger logger = LoggerLoader.getLogger(AbstractDAO.class);
 
-    protected DataSource dataSource = DataSource.getInstance();
+    protected MySqlDataSource dataSource = MySqlDataSource.getInstance();
 
 
     protected <T> T findById(String query, int id, Mapper<T> mapper) throws SQLException {
-        Connection connection = dataSource.getConnection();
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setInt(1, id);
-        ResultSet resultSet = statement.executeQuery();
-        if (resultSet.next()) {
-            return mapper.map(resultSet);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapper.map(resultSet);
+                }
+            }
         }
-
         return null;
     }
 
     protected int execute(String query) throws SQLException {
-        Connection connection = dataSource.getConnection();
-        Statement statement = connection.createStatement();
-        return statement.executeUpdate(query);
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            return statement.executeUpdate(query);
+        }
     }
 
     protected int getGeneratedKey(PreparedStatement statement) throws SQLException {
@@ -40,9 +39,6 @@ public abstract class AbstractDAO {
             if (resultSet.next()) {
                 return resultSet.getInt(1);
             }
-        } catch (SQLException e) {
-            logger.error("Failed to get generated key ", e);
-
         }
         throw new SQLException("Key is not generated");
     }
